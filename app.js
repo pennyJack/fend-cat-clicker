998f9d6efddba046c9ae317be0dd9298e6ff1b1e
Andre Wibbeke <andre.wibbeke@gmail.com>
2018-05-19 16:28:58 +0200

    feat: Add reset button

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const kitten = document.querySelector('img');
 const score = document.querySelector('.score');
+const button = document.querySelector('button');
 let counter = 0;
 
 kitten.addEventListener('click', function() {
   counter++;
   score.textContent = counter;
+});
+
+button.addEventListener('click', function(event) {
+  console.log(event);
+  window.location.reload();
 });
