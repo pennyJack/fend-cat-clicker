976cf7996e3dedb47512f96d3f4f48083c4d6a3b
Andre Wibbeke <andre.wibbeke@gmail.com>
2018-05-20 10:06:05 +0200

    refactor: Use closures for state management of counter

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const cats = document.getElementsByTagName('img');
 const scores = document.getElementsByClassName('score');
 const button = document.querySelector('button');
-let counter1 = 0;
-let counter2 = 0;
+const counter = 0;
 
-cats[0].addEventListener('click', function() {
-  counter1++;
-  scores[0].textContent = counter1;
-});
 
-cats[1].addEventListener('click', function() {
-  counter2++;
-  scores[1].textContent = counter2;
-});
+cats[0].addEventListener('click', (function(copyOfCounter) {
+  return function() {
+    copyOfCounter++;
+    scores[0].textContent = copyOfCounter;
+  }
+}(counter)));
+
+cats[1].addEventListener('click', (function(copyOfCounter) {
+  return function() {
+    copyOfCounter++;
+    scores[1].textContent = copyOfCounter;
+  }
+}(counter)));
 
 button.addEventListener('click', function() {
   window.location.reload();
