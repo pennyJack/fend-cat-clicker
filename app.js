4b64b9cb2d2d4a34d96edd49a250587cec7e3907
Andre Wibbeke <andre.wibbeke@gmail.com>
2018-05-20 10:23:17 +0200

    refactor: Use a loop to create Event Listeners for each cat image

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const cats = document.getElementsByTagName('img');
-const scores = document.getElementsByClassName('score');
+const cats = document.querySelectorAll('img');
+const scores = document.querySelectorAll('.score');
 const button = document.querySelector('button');
 const counter = 0;
 
+cats.forEach(function(cat, index) {
+  cat.addEventListener('click', (function(copyOfCounter, copyOfIndex) {
+    return function() {
+      copyOfCounter++;
+      scores[copyOfIndex].textContent = copyOfCounter;
+    }
+  }(counter, index)));
+});
 
-cats[0].addEventListener('click', (function(copyOfCounter) {
+/* cats[0].addEventListener('click', (function(copyOfCounter) {
   return function() {
     copyOfCounter++;
     scores[0].textContent = copyOfCounter;
@@ -16,7 +24,7 @@ cats[1].addEventListener('click', (function(copyOfCounter) {
     copyOfCounter++;
     scores[1].textContent = copyOfCounter;
   }
-}(counter)));
+}(counter))); */
 
 button.addEventListener('click', function() {
   window.location.reload();
