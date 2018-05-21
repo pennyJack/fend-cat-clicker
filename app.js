8bdf841fd504cee4b8e8cfd7bc7a80a8b091503e
Andre Wibbeke <andre.wibbeke@gmail.com>
2018-05-21 10:55:10 +0200

    feature: Finish Cat Clicker Premium version

--- app.js
+++ app.js
@@ -1,26 +1,64 @@
-const source = document.getElementById("thumb-template").innerHTML;
-const template = Handlebars.compile(source);
-const container = document.querySelector('.container');
+//handlebarsjs variables
+const thumbSrc = document.getElementById("thumb-template").innerHTML;
+const imageSrc = document.getElementById("image-template").innerHTML;
+const thumbTemp = Handlebars.compile(thumbSrc);
+const imageTemp = Handlebars.compile(imageSrc);
+
+//Custom global variables
+const thumbnailContainer = document.querySelector('.thumbnailContainer');
+const imageContainer = document.querySelector('.imageContainer');
 const cats = [];
 
 class Cat {
-  constructor(name, src) {
+  constructor(name, id, thumbnailURL, imageURL) {
     this.name = name;
-    this.src = src;
+    this.id = id;
+    this.thumbnailURL = thumbnailURL;
+    this.imageURL = imageURL;
+    this.score = 0;
+    this.hasEvtListener = false;
+  }
+  updateScore() {
+    this.score++;
   }
 }
 
-cats.push(new Cat("Amber", "img/thumbs/thumb1.jpg"));
-cats.push(new Cat("Djego", "img/thumbs/thumb2.jpg"));
-cats.push(new Cat("Wesley", "img/thumbs/thumb3.jpg"));
-cats.push(new Cat("Marabella", "img/thumbs/thumb4.jpg"));
-cats.push(new Cat("Alexander", "img/thumbs/thumb5.jpg"));
+cats.push(new Cat("Amber", "0", "img/thumbs/thumb1.jpg", "img/cat1.jpg"));
+cats.push(new Cat("Djego", "1", "img/thumbs/thumb2.jpg", "img/cat2.jpg"));
+cats.push(new Cat("Wesley", "2", "img/thumbs/thumb3.jpg", "img/cat3.jpg"));
+cats.push(new Cat("Marabella", "3", "img/thumbs/thumb4.jpg", "img/cat4.jpg"));
+cats.push(new Cat("Alexander", "4", "img/thumbs/thumb5.jpg", "img/cat5.jpg"));
 
+//Intial DOM setup
 cats.forEach(function(cat) {
-  container.innerHTML += template(cat);
+  thumbnailContainer.innerHTML += thumbTemp(cat);
 });
 
-function setEventListeners() {
+//imageContainer.innerHTML = imageTemp(cats[0]);
+
+function setEventListenersToThumbnails() {
+  const catThumbnails = document.querySelectorAll('.catThumbnail');
+  const counter = 0;
+  let catImage = document.querySelector('.catImage');
+  let score = document.querySelector('.score');
+
+  catThumbnails.forEach(function(thumbnail) {
+    thumbnail.addEventListener('click', function(evt) {
+      const targetId = evt.target.getAttribute("id");
+      imageContainer.innerHTML = imageTemp(cats[targetId]);
+      catImage = document.querySelector('.catImage');
+      catImage.addEventListener('click', (function() {
+        cats[catImage.getAttribute("id")].updateScore();
+        score = document.querySelector('.score');
+        score.textContent = cats[catImage.getAttribute("id")].score;
+      }));
+    });
+  });
+}
+
+setEventListenersToThumbnails();
+
+/*function setEventListeners() {
   const catImages = document.querySelectorAll('img');
   const scores = document.querySelectorAll('.score');
   const buttons = document.querySelectorAll('button');
@@ -44,4 +82,4 @@ function setEventListeners() {
   });
 }
 
-setEventListeners();
+setEventListeners();*/
