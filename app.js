f48f7180960bda204ff478ee6a624936dc587cce
Andre Wibbeke <andre.wibbeke@gmail.com>
2018-05-22 17:58:24 +0200

    refactor: Add cat data array to the model

--- app.js
+++ app.js
@@ -5,7 +5,15 @@ $(function() {
   const thumbTemp = Handlebars.compile(thumbSrc);
   const imageTemp = Handlebars.compile(imageSrc);
 
+  //Data model
   const model = {
+    cats: [
+      {name: "Amber", clickCount: 0,  thumbnailURL: "img/thumbs/thumb1.jpg", imageURL: "img/cat1.jpg" },
+      {name: "Djego", clickCount: 0,  thumbnailURL: "img/thumbs/thumb2.jpg", imageURL: "img/cat2.jpg" },
+      {name: "Wesley", clickCount: 0,  thumbnailURL: "img/thumbs/thumb3.jpg", imageURL: "img/cat3.jpg" },
+      {name: "Marabella", clickCount: 0,  thumbnailURL: "img/thumbs/thumb4.jpg", imageURL: "img/cat4.jpg" },
+      {name: "Alexander", clickCount: 0,  thumbnailURL: "img/thumbs/thumb5.jpg", imageURL: "img/cat5.jpg" }
+    ];
 
   };
 
